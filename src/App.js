1e4b0be8063ac6a0f84a97d419b286876338469d
tanmibts <31706737+tanmibts@users.noreply.github.com>
2024-01-14 22:21:46 -0800

    Recreate simple business card website (#1)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useEffect } from "react"
 
 function App() {
+
+  useEffect(() => {
+    document.title = "victoria"
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          this is my website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <link rel="icon" href="/favicon.ico"></link>
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300i,400&amp;display=swap" rel="stylesheet"></link>
       </header>
-    </div>
+      <body>
+        <div className="App-text">
+          <div className="App-div">
+            my name is victoria tan
+          </div>
+          <div className="App-div">
+            i am a software engineer at <a className="App-link" href="https://www.streetlightdata.com/" >streetlight</a>
+          </div>
+          <div className="App-div"><div className="App-dots">...</div></div>
+          <div className="App-div"><a className="App-link" href="mailto:victoria.em1281@gmail.com">victoria.em1281@gmail.com</a></div>
+          <div className="App-div"><a className="App-link" href="https://www.linkedin.com/in/victoria-t-a69777b6/">linkedin</a></div>
+          <div className="App-div"><a className="App-link" href="https://github.com/tanmibts/">github</a></div>
+        </div>
+      </body >
+    </div >
   );
 }
 
