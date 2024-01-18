7c5c5735cc525340792b6f6af535fd4860077d83
tanmibts <root@LAPTOP-K42K98PP.localdomain>
2024-01-17 18:55:25 -0800

    books

---
+++ src/Projects/BookRecs/BookRecs.tsx
@@ -0,0 +1,28 @@
+import { Dots } from "../../Misc/Dots";
+
+export const BookRecs = () => {
+    return <>
+
+        <div className="margin-xy">
+            <div>
+                here are some books that have shaped how I thinkfeel:
+            </div>
+            <div>- <b>jason hickel</b> less is more: how degrowth will save the world</div>
+            <div>- <b>anna tsing</b> the mushroom at the end of the world: on the possibility of life in capitalist ruins</div>
+            <div>- <b>robin wall kimmerer</b> braiding sweetgrass: indigenous wisdom, scientific knowledge, and the teachings of plants</div>
+            <div>- <b>abraham joshua heschel</b> the sabbath: its meaning for modern man</div>
+            <div>- <b>grace lee boggs</b> the next american revolution: sustainable activism for the twenty-first century</div>
+            <div>- <b>david r. montgomery</b> dirt: the erosion of civilizations</div>
+        </div >
+        <Dots />
+        <div className="margin-xy">
+            <div>and here are some books that have shaped how I feelthink:</div>
+            <div>- <b>john berger</b> and our faces, my heart, brief as photos</div>
+            <div>- <b>leone ross</b> come let us sing anyway</div>
+            <div>- <b>frank o'hara</b> lunch poems</div>
+            <div>- <b>forrest gander</b> be with</div>
+            <div>- <b>jenny zhang</b> my baby first birthday</div>
+        </div>
+
+    </>;
+}
