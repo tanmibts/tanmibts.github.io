8ce9aa5f881f36ab974b99e3e4fadd304602c8bb
tanmibts <31706737+tanmibts@users.noreply.github.com>
2024-01-17 18:56:46 -0800

    Add books (#4)

--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -1,9 +1,13 @@
 import { Wood } from "./Wood/Wood"
 // import { ZineSlappySlant } from "./ZineSlappySlant"
+import { BookRecs } from "./BookRecs/BookRecs"
+import { Dots } from "../Misc/Dots"
 
 export function Projects() {
     return <div>
         <Wood />
         {/* <ZineSlappySlant /> */}
+        <Dots />
+        <BookRecs />
     </div>
 }
